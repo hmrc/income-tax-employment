0b75f19a86fb9e591930b7bd3491a5a4c537e15f
michael-robert-poole <81170372+michael-rob-poole@users.noreply.github.com>
2021-06-04 11:15:52 +0100

    update payModel and relevant tests

--- it/connectors/GetEmploymentDataConnectorSpec.scala
+++ it/connectors/GetEmploymentDataConnectorSpec.scala
@@ -306,9 +306,7 @@ object GetEmploymentDataConnectorSpec {
       |    },
       |    "pay": {
       |      "taxablePayToDate": 34234.15,
-      |      "totalTaxToDate": 6782.92,
-      |      "payFrequency": "CALENDAR MONTHLY",
-      |      "paymentDate": "2020-04-23"
+      |      "totalTaxToDate": 6782.92
       |    }
       |  }
       |}
