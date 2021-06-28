3c465f871e0073c4bcea20ffc7a6dfd73ea2889b
JuamalBlackman <juamal.blackman@digital.hmrc.gov.uk>
2021-06-28 13:11:42 +0100

    [SASS-980] ignoreEmployments connector

---
+++ app/connectors/IgnoreEmploymentConnector.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.AppConfig
+import connectors.httpParsers.IgnoreEmploymentHttpParser.{IgnoreEmploymentResponse,IgnoreEmploymentHttpReads}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import utils.DESTaxYearHelper.desTaxYearConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class IgnoreEmploymentConnector @Inject()(val http: HttpClient,
+                                          val appConfig: AppConfig)(implicit ec:ExecutionContext) extends DesConnector {
+
+  def ignoreEmployment(nino: String, taxYear: Int, employmentId: String)
+                           (implicit hc: HeaderCarrier): Future[IgnoreEmploymentResponse] = {
+
+    val ignoreEmploymentUri: String =
+      appConfig.desBaseUrl + s"/income-tax/income/employments/$nino/${desTaxYearConverter(taxYear)}/$employmentId/ignore"
+
+    def desCall(implicit hc: HeaderCarrier): Future[IgnoreEmploymentResponse] = {
+      http.PUTString[IgnoreEmploymentResponse](ignoreEmploymentUri, "")
+    }
+
+    desCall(desHeaderCarrier(ignoreEmploymentUri))
+  }
+}
