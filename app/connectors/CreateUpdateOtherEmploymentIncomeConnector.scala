66eede808d7b3f56438d144c48647f0116f94f15
Roberto Tena Garcia <roberto.tena.garcia@digital.hmrc.gov.uk>
2023-06-01 13:22:08 +0100

    SASS-4736 Implement 1617 Create Update Other Employment

---
+++ app/connectors/CreateUpdateOtherEmploymentIncomeConnector.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.AppConfig
+import connectors.parsers.CreateUpdateOtherEmploymentsIncomeHttpParser.{CreateUpdateOtherEmploymentsIncomeHttpReads, CreateUpdateOtherEmploymentsIncomeResponse}
+import models.api.OtherEmploymentsIncome
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import utils.DESTaxYearHelper.desTaxYearConverter
+
+import java.net.URL
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class CreateUpdateOtherEmploymentIncomeConnector @Inject()(val http: HttpClient,
+                                                           val appConfig: AppConfig)(implicit ec: ExecutionContext) extends DesConnector {
+  def createUpdateOtherEmploymentIncome(nino: String,
+                                        taxYear: Int,
+                                        otherEmploymentsIncome: OtherEmploymentsIncome)
+                                       (implicit hc: HeaderCarrier): Future[CreateUpdateOtherEmploymentsIncomeResponse] = {
+    val url = new URL(s"$baseUrl/income-tax/income/other/employments/$nino/${desTaxYearConverter(taxYear)}")
+
+    def desCall(implicit hc: HeaderCarrier): Future[CreateUpdateOtherEmploymentsIncomeResponse] = {
+      http.PUT[OtherEmploymentsIncome, CreateUpdateOtherEmploymentsIncomeResponse](url, otherEmploymentsIncome)
+    }
+
+    desCall(desHeaderCarrier(url))
+  }
+}
