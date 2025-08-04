2d044cf4f255e004fd006c936baf262645d6c380
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2025-08-04 14:09:09 +0100

    MIPR-1948: Upgrade Submissions dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,8 +17,8 @@
 import sbt.*
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoPlay30Version = "2.6.0"
+  private val bootstrapVersion = "9.19.0"
+  private val hmrcMongoPlay30Version = "2.7.0"
 
   private val jacksonAndPlayExclusions: Seq[InclusionRule] = Seq(
     ExclusionRule(organization = "com.fasterxml.jackson.core"),
@@ -30,9 +30,9 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.19.1",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.19.2",
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoPlay30Version,
-    "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.2.0",
+    "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.3.0",
     "com.beachape"                  %% "enumeratum"                 % "1.9.0",
     "com.beachape"                  %% "enumeratum-play-json"       % "1.9.0" excludeAll (jacksonAndPlayExclusions *),
     "org.typelevel"                 %% "cats-core"                  % "2.13.0"
@@ -43,7 +43,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"  % hmrcMongoPlay30Version,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.8",
     "com.github.tomakehurst"  %  "wiremock-jre8-standalone" % "3.0.1",
-    "org.scalamock"           %% "scalamock"                % "7.3.3",
+    "org.scalamock"           %% "scalamock"                % "7.4.0",
     "org.mockito"             %% "mockito-scala"            % "2.0.0"
   ).map(_ % "test")
 }
